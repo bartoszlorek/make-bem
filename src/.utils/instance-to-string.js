b759654bd2490de3789be4e420b51131502f4d90
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-02 01:40:03 +0200

    simplify api

--- src/.utils/instance-to-string.js
+++
@@ -1,8 +0,0 @@
-function instanceToString(value, proto) {
-    if (value != null && value.instanceOf === proto) {
-        return value.toString()
-    }
-    return value
-}
-
-export default instanceToString
--- src/.utils/prefixed-join.js
+++
@@ -1,8 +0,0 @@
-function prefixedJoin(array, separator = ',') {
-    if (array != null && array.length) {
-        return separator + array.join(separator)
-    }
-    return ''
-}
-
-export default prefixedJoin
