3ca5efd00ee9d3a5511b4fcc4230d395d5aba33d
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-02 01:42:55 +0200

    redefine api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,64 @@
-import makeBem from './make-bem'
-module.exports = makeBem
+// the default naming scheme is Two Dashes style
+// block-name__elem-name--mod-name--mod-val
+
+import objectAssign from 'object-assign'
+import baseClassNames from './.internal/base-class-names'
+import joinClassNames from './.internal/join-class-names'
+import makeSelector from './.internal/make-selector'
+
+const defaults = {
+    prefix: '',
+    element: '__',
+    modifier: '--',
+    value: '--'
+}
+
+function makeBem(style, options) {
+    const spec = objectAssign({}, defaults, options)
+    const getSelector = makeSelector(spec)
+
+    const constructor = (block, element, modifiers, extras) => {
+        let _block = block || '',
+            _element = element || '',
+            _modifiers = {},
+            _extras = {}
+
+        const getEntity = (modifier, value) => {
+            let selector = getSelector(_block, _element, modifier, value)
+            return style != null ? style[selector] || selector : selector
+        }
+
+        const self = {
+            elem: (name, modifiers, extras) => {
+                return constructor(_block, name, modifiers, extras)
+            },
+            mod: (...args) => {
+                _modifiers = objectAssign({}, _modifiers, baseClassNames(args))
+                return self
+            },
+            extra: (...args) => {
+                _extras = objectAssign({}, _extras, baseClassNames(args))
+                return self
+            },
+            toString: () => {
+                return (
+                    getEntity() +
+                    joinClassNames(_modifiers, getEntity) +
+                    joinClassNames(_extras)
+                )
+            }
+        }
+
+        if (modifiers != null) {
+            self.mod(modifiers)
+        }
+        if (extras != null) {
+            self.extra(extras)
+        }
+        return self
+    }
+
+    return constructor
+}
+
+export default makeBem
--- test/make-bem.test.js
+++ test/make-bem.test.js
@@ -1,22 +1,79 @@
-import makeBem from '../src/make-bem'
+import makeBem from '../src/index'
 
-describe('make-bem.js', () => {
+describe('without style', () => {
+    const b = makeBem()
 
-    const style = {
+    it('should return clean block', () => {
+        expect(b('btn').toString()).toBe('btn')
+    })
+
+    it('should return own element', () => {
+        const label = b('btn').elem('label')
+        expect(label.toString()).toBe('btn__label')
+    })
+
+    it('should handle modifiers and extras', () => {
+        const btn = b('btn')
+            .mod('red', { big: true })
+            .extra('good', { add: true })
+        expect(btn.toString()).toBe('btn btn--red btn--big good add')
+    })
+
+    it('should handle modifiers value', () => {
+        const btn = b('btn').mod({ big: true, type: 'submit' })
+        expect(btn.toString()).toBe('btn btn--big btn--type--submit')
+    })
+})
+
+describe('with hashed style', () => {
+    // prettier-ignore
+    const b = makeBem({
         'button': '_btn1',
         'button--active': '_btna',
         'button--primary': '_btnp',
         'button__label': '_btn_l',
         'button__label--hover': '_btn_lh'
-    }
-    const bem = makeBem(style)
-
-    it('should handle multiple modifiers', () => {
-        let button = bem('button')
-            .mod('hover', false)
-            .mod('active');
-        expect(button.toString())
-            .toBe('_btn1 _btna')
     })
 
+    it('should return hashed block', () => {
+        expect(b('button').toString()).toBe('_btn1')
+    })
+
+    it('should return clean block', () => {
+        expect(b('undefined').toString()).toBe('undefined')
+    })
+
+    it('should return own element', () => {
+        const label = b('button').elem('label')
+        expect(label.toString()).toBe('_btn_l')
+    })
+
+    it('should handle modifiers', () => {
+        const btn = b('button').mod('primary', { active: true })
+        expect(btn.toString()).toBe('_btn1 _btnp _btna')
+    })
+
+    it("should handle own element's modifiers and extras", () => {
+        const label = b('button').elem('label', { hover: true }, 'good')
+        expect(label.toString()).toBe('_btn_l _btn_lh good')
+    })
+})
+
+describe('with custom delimiters', () => {
+    const b = makeBem(null, {
+        prefix: 'x-',
+        element: '_',
+        modifier: '-',
+        value: '---'
+    })
+
+    it('should return own element', () => {
+        const label = b('button').elem('label')
+        expect(label.toString()).toBe('x-button_label')
+    })
+
+    it('should handle modifiers', () => {
+        const btn = b('button').mod('primary', { active: true, type: 'submit' })
+        expect(btn.toString()).toBe('x-button x-button-primary x-button-active x-button-type---submit')
+    })
 })
