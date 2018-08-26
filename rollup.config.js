6bb1160803cdb50fbd0a710e6bfbfa869c8ca3de
Bartosz Lorek <bery.lorek@gmail.com>
2018-08-26 19:52:48 +0200

    add beautify format

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,23 @@ import commonjs from 'rollup-plugin-commonjs'
 import uglify from 'rollup-plugin-uglify'
 import babel from 'rollup-plugin-babel'
 
+const ENTRY = 'src/index.js'
+const UMD_NAME = 'makeBem'
+const FILENAME = 'make-bem'
+const EXTERNAL = []
+
+const uglifyConfig = {
+    compress: {
+        warnings: false,
+        drop_console: true,
+        drop_debugger: true,
+        reduce_funcs: false
+    },
+    mangle: {
+        toplevel: true
+    }
+}
+
 const babelConfig = {
     babelrc: false,
     exclude: 'node_modules/**',
@@ -10,40 +27,57 @@ const babelConfig = {
     plugins: ['external-helpers']
 }
 
-export default [
-    {
-        entry: 'src/index.js',
-        output: {
-            file: './dist/make-bem.js',
-            format: 'cjs'
-        },
-        plugins: [
-            resolve(),
-            commonjs(),
-            babel(babelConfig)
+const makeUglify = output => ({
+    entry: ENTRY,
+    output,
+    plugins: [
+        resolve(),
+        commonjs(),
+        uglify(uglifyConfig),
+        babel(babelConfig)
+    ],
+    external: EXTERNAL,
+    watch: { include: 'src/**' }
+})
+
+const makeBeautify = output => ({
+    entry: ENTRY,
+    output,
+    plugins: [
+        resolve(),
+        commonjs(),
+        babel(babelConfig)
+    ],
+    external: EXTERNAL,
+    watch: { include: 'src/**' }
+})
+
+const configs = (function() {
+    if (process.env.DEVELOPMENT) {
+        return [
+            makeBeautify({
+                file: `./dist/${FILENAME}.umd.js`,
+                format: 'umd',
+                name: UMD_NAME
+            })
         ]
-    },
-    {
-        entry: 'src/index.js',
-        output: {
-            file: './dist/make-bem.min.js',
-            format: 'cjs'
-        },
-        plugins: [
-            resolve(),
-            commonjs(),
-            uglify({
-                compress: {
-                    warnings: false,
-                    drop_console: true,
-                    drop_debugger: true,
-                    reduce_funcs: false
-                },
-                mangle: {
-                    toplevel: true
-                }
+    } else {
+        return [
+            makeBeautify({
+                file: `./dist/${FILENAME}.js`,
+                format: 'cjs'
             }),
-            babel(babelConfig)
+            makeUglify({
+                file: `./dist/${FILENAME}.min.js`,
+                format: 'cjs'
+            }),
+            makeUglify({
+                file: `./dist/${FILENAME}.umd.js`,
+                format: 'umd',
+                name: UMD_NAME
+            })
         ]
     }
-]
+})()
+
+export default configs
